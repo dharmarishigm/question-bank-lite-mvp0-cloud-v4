2e19438980f1e5cba4b7382c49ec3894b0a68223
dharmapuri611143 <dharma.veera611143@gmail.com>
2026-09-06 14:25:02 +0530

    Use valid Alembic revision identifier

--- migrations/versions/0003_registration_attempt_override.py
+++ migrations/versions/0003_registration_attempt_override.py
@@ -1,7 +1,7 @@
 """Add per-registration attempt allowance."""
 from alembic import op
 
-revision = "0003_registration_attempt_override"
+revision = "0003_attempt_override"
 down_revision = "0002_exam_blueprints"
 branch_labels = None
 depends_on = None
